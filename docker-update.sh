f0cf1ba44695fbef2c5ad5a997bcba906c872568
Maximiliano Merlo <maximerlo.1989@gmail.com>
2024-08-12 18:14:32 -0300

    Update docker-update.sh

--- docker-update.sh
+++ docker-update.sh
@@ -2,7 +2,7 @@
 aws ecr get-login-password --region {AWS_REGION} | sudo docker login --username AWS --password-stdin {ECR_REPO}
 
 # Detenemos el contenedor
-sudo docker-compose -f {PROJECT_PATH}/docker-compose.yml down 2>&1
+sudo {PROJECT_PATH}/docker-compose down 2>&1
 
 # Limpiamos las imágenes que sean /${IMAGE_NAME}
 sudo docker rmi $(sudo docker images | grep {IMAGE_NAME} | awk '{print $3}') -f 2>&1
@@ -11,7 +11,7 @@ sudo docker rmi $(sudo docker images | grep {IMAGE_NAME} | awk '{print $3}') -f 
 # sudo docker rm $(sudo docker ps -a | grep ${IMAGE_NAME} | awk '{print $1}') -f 2>&1
 
 # Volvemos a ejecutar el docker-compose
-sudo docker-compose -f {PROJECT_PATH}/docker-compose.yml up --build -d 2>&1
+sudo {PROJECT_PATH}/docker-compose up --build -d 2>&1
 
 # Listo
 echo "Actualización completada"
